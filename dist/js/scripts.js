3412145671b6b7a7bda58fb4ab91b2f6349ab504
David Miller <hello@davidmiller.io>
2021-10-18 17:25:34 -0400

    dependencies update

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Bare v5.0.6 (https://startbootstrap.com/template/bare)
+* Start Bootstrap - Bare v5.0.7 (https://startbootstrap.com/template/bare)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
 */
